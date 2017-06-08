834c5ad8a98198669f943f5e3a06c038bfaeb501
Hyun Frantz <hyunfrantz@gmail.com>
2017-06-08 01:07:40 -0400

    Query the data

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -8,6 +8,14 @@ module ExpenseTracker
     def app
       ExpenseTracker::API.new
     end
+
+    def post_expense(expense)
+      post '/expenses',JSON.generate(expense)
+      expect(last_response.status).to eq(200)
+      parsed = JSON.parse(last_response.body)
+      expect(parsed).to include('expense_id'=>a_kind_of(Integer))
+      expense.merge('id'=>parsed['expense_id'])
+    end
     it 'records sumitted expenses' do
       coffee = {
         'payee' => 'Star Bucks',
