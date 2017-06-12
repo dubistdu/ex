0b63d0b13bd86fbcaa25c4e466352a3c28f63ba9
Hyun Frantz <hyunfrantz@gmail.com>
2017-06-11 23:50:49 -0400

    added hard coded return value

--- app/api.rb
+++ app/api.rb
@@ -4,6 +4,10 @@ require 'json'
 module ExpenseTracker
   class API < Sinatra::Base
     post '/expenses' do
+      JSON.generate('expense_id' => 42)
+    end
+    get 'expenses/:date' do
+      JSOn.generate([])
     end
   end
 end
--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -2,35 +2,53 @@ require 'rack/test'
 require 'json'
 require_relative '../../app/api'
 
+
 module ExpenseTracker
   RSpec.describe 'Expense Tracker API' do
     include Rack::Test::Methods
+
     def app
       ExpenseTracker::API.new
     end
 
     def post_expense(expense)
-      post '/expenses',JSON.generate(expense)
+      post '/expenses', JSON.generate(expense)
       expect(last_response.status).to eq(200)
+
       parsed = JSON.parse(last_response.body)
-      expect(parsed).to include('expense_id'=>a_kind_of(Integer))
-      expense.merge('id'=>parsed['expense_id'])
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expense.merge('id' => parsed['expense_id'])
     end
-    it 'records sumitted expenses' do
-      coffee = {
-        'payee' => 'Star Bucks',
+
+    it 'records submitted expenses' do
+      coffee = post_expense(
+        'payee'  => 'Starbucks',
         'amount' => 5.75,
-        'date' => '2017-06-10'
-      }
-      post '/expenses', JSON.generate(coffee)
+        'date'   => '2017-06-10'
+      )
+
+      zoo = post_expense(
+        'payee'  => 'Zoo',
+        'amount' => 15.25,
+        'date'   => '2017-06-10'
+      )
+
+      groceries = post_expense(
+        'payee'  => 'Whole Foods',
+        'amount' => 95.20,
+        'date'   => '2017-06-11'
+      )
+      # POST coffee, zoo, and groceries expenses here
+
+      get '/expenses/2017-06-10'
       expect(last_response.status).to eq(200)
 
-      parsed = JSON.parse(last_response.body)
-      expect(parsed).to include('expense_id'=> a_kind_of(Integer))
-
+      expenses = JSON.parse(last_response.body)
+      expect(expenses).to contain_exactly(coffee, zoo)
     end
   end
 end
+
 
 # Because  JSON object convert to Ruby hashs, our example also will have string keys
 # ex) instead of {payee: 'Star Bucks'}, it's {'payee' => 'Star Bucks'}
